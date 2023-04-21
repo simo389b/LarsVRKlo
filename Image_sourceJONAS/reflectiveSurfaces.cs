33b4e499d434b66018f03b0cd75869a75525d8c4
Jonas <jonasemil0305@gmail.com>
2023-04-21 10:05:08 +0200

    ISM. Virker med andenordens og næsten med trejde
    Mangler vægidentifikation, Simons Unity-import og gennemtest

---
+++ Image_sourceJONAS/reflectiveSurfaces.cs
@@ -0,0 +1,44 @@
+using System;
+
+public class Program {
+    public static void Main() {
+        double[] point = new double[] {2,2,2};
+
+        Room room = new Room();
+        double[,,] wallVertices = room.GetWalls();
+        
+        WallVectors wallVectors = new WallVectors(wallVertices);
+        double[,,] vectors = wallVectors.GetVectors();
+        
+        WallNormals wallNormals = new WallNormals(vectors);
+        double[,] normals = wallNormals.GetNormals();
+        
+        FirstImageSources firstImageSource = new FirstImageSources(point, normals, wallVertices);
+        Tuple<double[,],double[,]> firstISMResults = firstImageSource.GetFirstImageSourcesAndProj();
+        double[,] firstImageSources = firstISMResults.Item1;
+        double[,] firstImageProjs = firstISMResults.Item2;
+
+        Console.WriteLine("The first-order imagesources of the reflective surfaces are:");
+        for (int i = 0; i < firstImageSources.GetLength(0); i++) {
+            Console.WriteLine("Image {0}: ({1}, {2}, {3})", i + 1, firstImageSources[i, 0], firstImageSources[i, 1], firstImageSources[i,2]);
+        }
+
+        Console.WriteLine("Her kommer andenordens. aj-aj hr. kaptajn");
+        SecondImageSources secondImageSource = new SecondImageSources(firstImageSources, normals, wallVertices, point);
+        double[,] secondImageSources = secondImageSource.GetSecondImageSources();
+        for (int i = 0; i < secondImageSources.GetLength(0); i++) {
+            Console.WriteLine("Image {0}: ({1}, {2}, {3})", i + 1, secondImageSources[i, 0], secondImageSources[i, 1], secondImageSources[i,2]);
+        }
+
+        Console.WriteLine("Her kommer tredjeordens. aj-aj hr. kaptajn");
+        ThirdImageSources thirdImageSource = new ThirdImageSources(secondImageSources, firstImageProjs, point, firstImageSources);
+        double[,] thirdImageSources = thirdImageSource.GetThirdImageSources();
+        for (int i = 0; i < thirdImageSources.GetLength(0); i++) {
+            Console.WriteLine("Image {0}: ({1}, {2}, {3})", i + 1, thirdImageSources[i, 0], thirdImageSources[i, 1], thirdImageSources[i,2]);
+        }
+
+    }
+}
+
+
+
